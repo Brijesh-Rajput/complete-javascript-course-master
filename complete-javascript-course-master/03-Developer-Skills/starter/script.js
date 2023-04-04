88727e57013f93b48641dc8e34be1986516c1bcb
Brijesh Rajput <brijeshrajput563@gmail.com>
2023-04-05 02:45:29 +0530

    Completed Guess My Number Project

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// Prettier --> extension. Converts every single quotes into double quotes.
+
+/*
+node -v  //to see the version of nodejs
+npm install live-server -g //g represent that globally live-server tool will install by npm library
+live-server //to start live-server.
+*/
